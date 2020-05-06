138b7e73e83cdf5c212ed4adbccfb7abe4722a5d
iammantas <nacickasmantas@gmail.com>
2020-05-06 12:42:44 +0300

    added new title

--- src/main/java/com/amigoscode/demo/DemoApplication.java
+++ src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -9,5 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("cccc");
+        System.out.println("ttttt");
+        System.out.println("bbbb");
     }
 }
