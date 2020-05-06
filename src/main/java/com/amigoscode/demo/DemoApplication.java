5e570fba84bfa50dc6da75220a19bb3f0f700b10
Mantas Nacickas <59713630+mantas462@users.noreply.github.com>
2020-05-06 12:32:48 +0300

    Update DemoApplication.java

--- src/main/java/com/amigoscode/demo/DemoApplication.java
+++ src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -8,5 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+                System.out.println("BBBB");
+
     }
 }
