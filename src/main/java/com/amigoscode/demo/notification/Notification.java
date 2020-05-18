30ec5a461f033382d8542de9bf014def1e9afdc5
manolio99 <mplanells84@gmail.com>
2020-05-18 16:39:37 +0200

    added message property

--- src/main/java/com/amigoscode/demo/notification/Notification.java
+++ src/main/java/com/amigoscode/demo/notification/Notification.java
@@ -1,4 +1,17 @@
 package com.amigoscode.demo.notification;
 
 public class Notification {
+    String message;
+
+    public Notification(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
