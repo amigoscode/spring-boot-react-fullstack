c3cd75368eea329342190b54a58360e83c13c98b
manolio99 <mplanells84@gmail.com>
2020-05-18 16:52:37 +0200

    deleting constructor

--- src/main/java/com/amigoscode/demo/notification/Notification.java
+++ src/main/java/com/amigoscode/demo/notification/Notification.java
@@ -3,10 +3,6 @@ package com.amigoscode.demo.notification;
 public class Notification {
     String message;
 
-    public Notification(String message) {
-        this.message = message;
-    }
-
     public String getMessage() {
         return message;
     }
